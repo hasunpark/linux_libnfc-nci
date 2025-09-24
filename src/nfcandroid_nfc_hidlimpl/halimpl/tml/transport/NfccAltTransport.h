2e06138e8e9dddcd1cde3f07ca6ce0aa0c0c41eb
Hasun Park <hasunpark@gmail.com>
2025-09-25 02:24:11 +0900

    Update default VEN/IRQ GPIO pins

--- src/nfcandroid_nfc_hidlimpl/halimpl/tml/transport/NfccAltTransport.h
+++ src/nfcandroid_nfc_hidlimpl/halimpl/tml/transport/NfccAltTransport.h
@@ -25,8 +25,8 @@
 #define I2C_ADDRESS 0x28
 #define I2C_BUS "/dev/i2c-1"
 #define SPI_BUS "/dev/spidev0.0"
-#define PIN_INT 23
-#define PIN_ENABLE 24
+#define PIN_INT 13
+#define PIN_ENABLE 20
 #define PIN_FWDNLD 25
 #define EDGE_NONE 0
 #define EDGE_RISING 1
